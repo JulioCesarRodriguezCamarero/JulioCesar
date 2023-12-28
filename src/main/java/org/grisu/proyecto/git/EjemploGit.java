af286f585d6dcbf263ad6d00e03c6e40088e16e3
Julius Kaesar <j3dianas@gmail.com>
2023-12-28 20:14:08 +0100

    segundo  comit

--- src/main/java/org/grisu/proyecto/git/EjemploGit.java
+++ src/main/java/org/grisu/proyecto/git/EjemploGit.java
@@ -3,5 +3,6 @@ package org.grisu.proyecto.git;
 public class EjemploGit {
     public static void main(String[] args) {
         System.out.println("Hola GIT");
+        System.out.println("que tal va la cosa?");
     }
 }
